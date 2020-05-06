fc957cc147f8703712582166d737a1e873f5e51b
neotao <neotao@126.com>
2020-05-06 10:16:51 +0800

    添加todo注释

--- maven-archetype/src/main/java/com/sparrow/ArchetypeApplication.java
+++ maven-archetype/src/main/java/com/sparrow/ArchetypeApplication.java
@@ -2,7 +2,9 @@ package com.sparrow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * todo 修改Application名
+ */
 @SpringBootApplication
 public class ArchetypeApplication {
 
--- maven-archetype/src/test/java/com/sparrow/ArchetypeApplicationTests.java
+++ maven-archetype/src/test/java/com/sparrow/ArchetypeApplicationTests.java
@@ -4,7 +4,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
+/**
+ * todo 修改Tests名
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ArchetypeApplicationTests {
